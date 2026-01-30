f5869f0a2dd0cab8f7e70a166cd2a96210a798e2
Darío Finelli <61764276+dariofin@users.noreply.github.com>
2026-01-30 19:04:27 -0300

    Layout portada cerrado (estructura + responsive)

---
+++ cms/administrator/logs/joomla_scheduler.php
@@ -0,0 +1,11 @@
+#
+#<?php die('Forbidden.'); ?>
+#Date: 2026-01-30 22:03:16 UTC
+#Software: Joomla! 5.4.2 Stable [ Kutegemea ] 6-January-2026 16:00 GMT
+
+#Fields: date	time	priority	message
+2026-01-30	22:03:16	INFO	Running task#02 'Session GC'.
+2026-01-30	22:03:16	INFO	Task> SessionGC end
+2026-01-30	22:03:16	INFO	Successfully finished task#02 in 0.00 (net 0.00) seconds.
+2026-01-30	22:03:26	INFO	Running task#03 'Update Notification'.
+2026-01-30	22:03:26	INFO	Successfully finished task#03 in 0.32 (net 0.32) seconds.
