2c1940472369f8f3a099916533e5a2a638e3afd2
Darío Finelli <61764276+dariofin@users.noreply.github.com>
2026-02-10 18:13:10 -0300

    agregado sección ADVENTURE y nuevas fotos

--- cms/administrator/components/com_akeebabackup/backup/akeeba.log.php
+++ cms/administrator/components/com_akeebabackup/backup/akeeba.log.php
@@ -67,3 +67,38 @@ DEBUG   |20260203 20:22:12|-- Loading optional filter Stack\StackHoststats
 DEBUG   |20260203 20:22:12|-- Loading optional filter Stack\StackErrorlogs
 DEBUG   |20260203 20:22:12|-- Loading optional filter Stack\StackFinder
 DEBUG   |20260203 20:22:12|-- Loading optional filter Stack\StackActionlogs
+DEBUG   |20260210 02:27:39| -- Resetting Akeeba Engine factory (backend.id-20260210-022739-950411)
+DEBUG   |20260210 02:27:39|Fetching filter data from database
+DEBUG   |20260210 02:27:39|Loading filters
+DEBUG   |20260210 02:27:39|-- Loading filter Regexfiles
+DEBUG   |20260210 02:27:39|-- Loading filter Extradirs
+DEBUG   |20260210 02:27:39|-- Loading filter Tabledata
+DEBUG   |20260210 02:27:39|-- Loading filter Directories
+DEBUG   |20260210 02:27:39|-- Loading filter Regextabledata
+DEBUG   |20260210 02:27:39|-- Loading filter Incremental
+DEBUG   |20260210 02:27:39|-- Loading filter Tablesalwaysskipped
+DEBUG   |20260210 02:27:39|-- Loading filter Regexdirectories
+DEBUG   |20260210 02:27:39|-- Loading filter Files
+DEBUG   |20260210 02:27:39|-- Loading filter Skipdirs
+DEBUG   |20260210 02:27:39|-- Loading filter Regextables
+DEBUG   |20260210 02:27:39|-- Loading filter Multidb
+DEBUG   |20260210 02:27:39|-- Loading filter Tables
+DEBUG   |20260210 02:27:39|-- Loading filter Regexskipdirs
+DEBUG   |20260210 02:27:39|-- Loading filter Regexskipfiles
+DEBUG   |20260210 02:27:39|-- Loading filter Skipfiles
+DEBUG   |20260210 02:27:39|-- Loading filter Excludefiles
+DEBUG   |20260210 02:27:39|-- Loading filter Cvsfolders
+DEBUG   |20260210 02:27:39|-- Loading filter Publicfolder
+DEBUG   |20260210 02:27:39|-- Loading filter Excludefolders
+DEBUG   |20260210 02:27:39|-- Loading filter Sitedb
+DEBUG   |20260210 02:27:39|-- Loading filter Systemcachefiles
+DEBUG   |20260210 02:27:39|-- Loading filter Libraries
+DEBUG   |20260210 02:27:39|-- Loading filter Excludetabledata
+DEBUG   |20260210 02:27:39|-- Loading filter Joomlaskipdirs
+DEBUG   |20260210 02:27:39|-- Loading filter Siteroot
+DEBUG   |20260210 02:27:39|-- Loading filter Joomlaskipfiles
+DEBUG   |20260210 02:27:39|Loading optional filters
+DEBUG   |20260210 02:27:39|-- Loading optional filter Stack\StackHoststats
+DEBUG   |20260210 02:27:39|-- Loading optional filter Stack\StackErrorlogs
+DEBUG   |20260210 02:27:39|-- Loading optional filter Stack\StackFinder
+DEBUG   |20260210 02:27:39|-- Loading optional filter Stack\StackActionlogs
