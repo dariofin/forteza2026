f85330a4c0d68d6b1871b706e3057b37e7f6cd1a
Darío Finelli <61764276+dariofin@users.noreply.github.com>
2026-02-11 02:37:21 -0300

    cambio de arquitectura

---
+++ _obsolete/cms/cache/com_virtuemart_orderby_manus/8955b5ebaf8e2e49f3b1e0f340801800-cache-com_virtuemart_orderby_manus-ebfb1f3c940ecf8cac29e6f0b9c6f82c.php
@@ -0,0 +1,10 @@
+<?php die("Access Denied"); ?>#x#a:2:{s:6:"result";s:795:"<label class="form-label mb-1" for="vm-orderby-select-mf">Select manufacturer</label>
+
+<select class="form-select mw-100" id="vm-orderby-select-mf">
+			
+								<option value="/forteza2026/cms/index.php/component/virtuemart/manufacturer/default?Itemid=101&customfields[100]=StichedVMlogo">Default</option>
+								<option value="/forteza2026/cms/index.php/component/virtuemart/manufacturer/manufacturer?Itemid=101&customfields[100]=StichedVMlogo">Manufacturer</option>
+								<option value="/forteza2026/cms/index.php/component/virtuemart/manufacturer/producer?Itemid=101&customfields[100]=StichedVMlogo">Producer</option>
+		
+		<option value="/forteza2026/cms/index.php/component/virtuemart?customfields[100]=Stiched+VM+logo&Itemid=101" selected="selected">All Manufacturers</option>
+	</select>";s:6:"output";s:0:"";}
